268bcc86ed70ee44e17269d1957d7bd0015b2e47
qinm08 <qinm08@gmail.com>
2017-11-22 16:42:04 +0800

    add maven pom and main application

---
+++ src/main/java/com/qinming/open/fastdfs/FastdfsClientApplication.java
@@ -0,0 +1,14 @@
+package com.qinming.open.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by QinMing on 2017/11/22.
+ */
+@SpringBootApplication
+public class FastdfsClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FastdfsClientApplication.class, args);
+    }
+}
